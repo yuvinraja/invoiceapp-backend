7afb75c1b046fd67ffa5392d978c64b642775524
Yuvin Raja <yuvinrajav@gmail.com>
2025-05-21 23:03:13 +0530

    create empty files

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@
 // This file sets up the Express application with middleware and routes.
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 // Middleware
+app.use(cors({origin: true, credentials: true}));
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
