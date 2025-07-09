76dad339c1963dddd61c4e8ede03af272f434e36
Yuvin Raja <yuvinrajav@gmail.com>
2025-07-09 23:54:47 +0530

    deployment fix #4

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ dotenv.config();
 
 const PORT = parseInt(process.env.PORT || "3000", 10);
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
